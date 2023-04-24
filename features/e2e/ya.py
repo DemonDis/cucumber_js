15daf3c1d937eadf12889232f77d360b83495b98
DemonDis <50297797+DemonDis@users.noreply.github.com>
2023-04-24 21:14:50 +0700

    ✨ print + input

--- features/e2e/ya.py
+++ features/e2e/ya.py
@@ -1,9 +1,10 @@
 from splinter import Browser
 from pytest_bdd import given, scenario, then, when
 import time
-# import logging
+# from selenium.webdriver.common.keys import Keys
 
 browser = Browser('firefox')
+# l = browser.find_element_by_class_name("search3__input mini-suggest__input")
 
 @scenario('ya.feature', 'посещаю фронт')
 def test_посещаю_фронт():
@@ -15,11 +16,16 @@ def test_посещаю_фронт():
 def _():
     """я ya.ru."""
     browser.visit("http://www.ya.ru")
-    print("No, it wasn't found... We need to improve our SEO techniques")
+    print("No, it wasn't found... We need to improve our SEO techniques") 
+    # browser.find_by_css('div[class="medium-widget success-story-category last"]:nth-child(2)').fill('splinter - python acceptance testing for web applications')
+    browser.find_by_css('input[class="search3__input mini-suggest__input"]').fill('splinter - python acceptance testing for web applications')
+    
     time.sleep(10) #sleep for 10 sec
     # browser.screenshot()
+    # l.send_keys("Selenium")
     print("Yes, found it! :)")
     browser.quit()
+
 
 # @when('я смотрю на картику')
 # def _():
